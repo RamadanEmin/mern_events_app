5671243b654c7db9f1f2d2918af9775da78f17e9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-11 21:55:52 +0300

    Added Button component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,6 +1,7 @@
-import { SignedIn, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 
 const Header = () => {
     return (
@@ -17,6 +18,13 @@ const Header = () => {
                     <SignedIn>
                         <UserButton afterSignOutUrl="/" />
                     </SignedIn>
+                    <SignedOut>
+                        <Button asChild className="rounded-full" size="lg">
+                            <Link href="/sign-in">
+                                Login
+                            </Link>
+                        </Button>
+                    </SignedOut>
                 </div>
             </div>
         </header>
