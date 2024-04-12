e585f679c119cd2e396dbba6978f635f8771e993
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-12 16:47:45 +0300

    Added NavItem component

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,6 +2,7 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import NavItems from './NavItems';
 
 const Header = () => {
     return (
@@ -13,6 +14,12 @@ const Header = () => {
                         alt="Evently logo"
                     />
                 </Link>
+
+                <SignedIn>
+                    <nav className="md:flex-between hidden w-full max-w-xs">
+                        <NavItems />
+                    </nav>
+                </SignedIn>
 
                 <div className="flex w-32 justify-end gap-3">
                     <SignedIn>
