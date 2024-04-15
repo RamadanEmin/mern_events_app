a84e9614684a8af0145f367b8f8ba4a4887da5af
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-15 15:29:02 +0300

    Change route

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -79,6 +79,7 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'OK', user: newUser });
     }
 
+
     if (eventType === 'user.updated') {
         const { id, image_url, first_name, last_name, username } = evt.data;
 
