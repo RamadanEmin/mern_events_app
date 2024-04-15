8d7699f7e6b62587ee2f9e15d41574dd45c3f4d4
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-15 17:23:04 +0300

    Added Form component

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -79,7 +79,6 @@ export async function POST(req: Request) {
         return NextResponse.json({ message: 'OK', user: newUser });
     }
 
-
     if (eventType === 'user.updated') {
         const { id, image_url, first_name, last_name, username } = evt.data;
 
