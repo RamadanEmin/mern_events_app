329899a9fcb293b856d22addaca361f9ed8a0cd4
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-15 20:28:38 +0300

    Added Event form component

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,6 +1,67 @@
-const EventForm = ({ userId, type }) => {
+'use client';
 
-    return ();
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import * as z from 'zod';
+import { eventDefaultValues } from '@/constants';
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: 'Username must be at least 2 characters.',
+    })
+});
+
+
+type EventFormProps = {
+    userId: string
+    type: "Create" | "Update"
+}
+
+const EventForm = ({ userId, type }: EventFormProps) => {
+    const initialValues = eventDefaultValues;
+
+    const form = useForm<z.infer>({
+        resolver: zodResolver(eventFormSchema),
+        defaultValues: initialValues
+    });
+
+    async function onSubmit(values: z.infer) {
+
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <Input placeholder="Event title" {...field} className="input-field" />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="categoryId"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+            </form>
+        </Form>
+    );
 }
 
 export default EventForm;
