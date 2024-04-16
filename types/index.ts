574d3e1abc7ccfdb944ed866cbba55243e81260e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-16 22:16:35 +0300

    Added Create event action

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,4 @@
 // USER PARAMS
-
 export type CreateUserParams = {
     clerkId: string
     firstName: string
@@ -14,6 +13,24 @@ export type UpdateUserParams = {
     lastName: string
     username: string
     photo: string
+}
+
+// EVENT PARAMS
+export type CreateEventParams = {
+    userId: string
+    event: {
+        title: string
+        description: string
+        location: string
+        imageUrl: string
+        startDateTime: Date
+        endDateTime: Date
+        categoryId: string
+        price: string
+        isFree: boolean
+        url: string
+    }
+    path: string
 }
 
 // CATEGORY PARAMS
