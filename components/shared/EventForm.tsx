44d9595887003b7f6a113725500aa98e3b57a961
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-18 16:27:34 +0300

    Added location in event form

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
@@ -124,6 +125,30 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                                         imageUrl={field.value}
                                         setFiles={setFiles}
                                     />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+                    <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem className="w-full">
+                                <FormControl>
+                                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                                        <Image
+                                            src="/assets/icons/location-grey.svg"
+                                            alt="calendar"
+                                            width={24}
+                                            height={24}
+                                        />
+
+                                        <Input placeholder="Event location or Online" {...field} className="input-field" />
+                                    </div>
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
