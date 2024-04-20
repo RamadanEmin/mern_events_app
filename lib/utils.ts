7377da92a06faabe97343b4f99de477a6750ee2f
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-20 10:51:43 +0300

    Added Format Date and Time function

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,12 +2,48 @@ import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export const formatDateTime = (dateString: Date) => {
+    const dateTimeOptions: Intl.DateTimeFormatOptions = {
+        weekday: 'short',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    }
+
+    const dateOptions: Intl.DateTimeFormatOptions = {
+        weekday: 'short',
+        month: 'short',
+        year: 'numeric',
+        day: 'numeric',
+    }
+
+    const timeOptions: Intl.DateTimeFormatOptions = {
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: true,
+    }
+
+    const formattedDateTime: string = new Date(dateString).toLocaleString('en-US', dateTimeOptions)
+
+    const formattedDate: string = new Date(dateString).toLocaleString('en-US', dateOptions)
+
+    const formattedTime: string = new Date(dateString).toLocaleString('en-US', timeOptions)
+
+    return {
+        dateTime: formattedDateTime,
+        dateOnly: formattedDate,
+        timeOnly: formattedTime,
+    }
 }
 
 export const convertFileToUrl = (file: File) => URL.createObjectURL(file);
 
 export const handleError = (error: unknown) => {
-  console.error(error)
-  throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
+    console.error(error)
+    throw new Error(typeof error === 'string' ? error : JSON.stringify(error))
 }
