ddfbfd3ca232227ac5b097b71db5030b24767b57
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-21 17:32:56 +0300

    Added Delete confirmation functionality

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { IEvent } from '@/lib/database/models/event.model';
 import { formatDateTime } from '@/lib/utils';
+import { DeleteConfirmation } from './DeleteConfirmation';
 
 type CardProps = {
     event: IEvent,
@@ -13,6 +14,9 @@ type CardProps = {
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
     const { sessionClaims } = auth();
+    const userId = sessionClaims?.userId as string;
+
+    const isEventCreator = userId === event.organizer._id.toString();
 
     return (
         <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
@@ -21,6 +25,16 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
                 style={{ backgroundImage: `url(${event.imageUrl})` }}
                 className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
             />
+
+            {isEventCreator && !hidePrice && (
+                <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+                    <Link href={`/events/${event._id}/update`}>
+                        <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+                    </Link>
+
+                    <DeleteConfirmation eventId={event._id} />
+                </div>
+            )}
 
             <div className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4">
                 {!hidePrice && <div className="flex gap-2">
