37904354cc708d954047e3f00533d57f18ec3ab9
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-21 14:17:18 +0300

    Fetch All events

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
+import { getAllEvents } from '@/lib/actions/event.actions';
 
 export default async function Home() {
+
+    const events = await getAllEvents({
+        query: '',
+        category: '',
+        page: 1,
+        limit: 6
+    });
 
     return (
         <>
@@ -30,6 +38,16 @@ export default async function Home() {
 
             <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
                 <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+                <Collection
+                    data={events?.data}
+                    emptyTitle="No Events Found"
+                    emptyStateSubtext="Come back later"
+                    collectionType="All_Events"
+                    limit={6}
+                    page={1}
+                    totalPages={2}
+                />
             </section>
         </>
     )
