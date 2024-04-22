88bcda87a16b56abf9f04560612e45d1e6bebbf6
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-22 16:54:37 +0300

    Added related events in details page

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -1,10 +1,17 @@
-import { getEventById } from '@/lib/actions/event.actions'
+import Image from 'next/image';
+import { getEventById, getRelatedEventsByCategory } from '@/lib/actions/event.actions'
 import { formatDateTime } from '@/lib/utils';
 import { SearchParamProps } from '@/types'
-import Image from 'next/image';
+import Collection from '@/components/shared/Collection';
 
-const EventDetails = async ({ params: { id } }: SearchParamProps) => {
+const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) => {
     const event = await getEventById(id);
+
+    const relatedEvents = await getRelatedEventsByCategory({
+        categoryId: event.category._id,
+        eventId: event._id,
+        page: searchParams.page as string,
+    });
 
     return (
         <>
@@ -67,6 +74,20 @@ const EventDetails = async ({ params: { id } }: SearchParamProps) => {
                         </div>
                     </div>
                 </div>
+            </section>
+
+            <section className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+                <h2 className="h2-bold">Related Events</h2>
+
+                <Collection
+                    data={relatedEvents?.data}
+                    emptyTitle="No Events Found"
+                    emptyStateSubtext="Come back later"
+                    collectionType="All_Events"
+                    limit={3}
+                    page={searchParams.page as string}
+                    totalPages={relatedEvents?.totalPages}
+                />
             </section>
         </>
     )
