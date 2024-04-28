40eb3ec94932c7864b24984d5a9ba2a23ddc1455
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-28 22:26:28 +0300

    Added removeKeysFromQuery function

--- lib/utils.ts
+++ lib/utils.ts
@@ -60,6 +60,22 @@ export function formUrlQuery({ params, key, value }: UrlQueryParams) {
     );
 }
 
+export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
+    const currentUrl = qs.parse(params);
+
+    keysToRemove.forEach(key => {
+        delete currentUrl[key];
+    })
+
+    return qs.stringifyUrl(
+        {
+            url: window.location.pathname,
+            query: currentUrl,
+        },
+        { skipNull: true }
+    );
+}
+
 export const handleError = (error: unknown) => {
     console.error(error);
     throw new Error(typeof error === 'string' ? error : JSON.stringify(error));
