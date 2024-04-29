39e5b94cae36c7d6fe97211aa79284acb6c244df
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-29 21:51:51 +0300

    Update order pag

--- app/(root)/orders/page.tsx
+++ app/(root)/orders/page.tsx
@@ -1,7 +1,15 @@
 import Search from '@/components/shared/Search';
+import { getOrdersByEvent } from '@/lib/actions/order.actions';
+import { formatDateTime, formatPrice } from '@/lib/utils';
 import { SearchParamProps } from '@/types';
+import { IOrderItem } from '@/lib/database/models/order.model';
 
 const Orders = async ({ searchParams }: SearchParamProps) => {
+    const eventId = (searchParams?.eventId as string) || '';
+    const searchText = (searchParams?.query as string) || '';
+
+    const orders = await getOrdersByEvent({ eventId, searchString: searchText });
+
     return (
         <>
             <section className=" bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -24,6 +32,33 @@ const Orders = async ({ searchParams }: SearchParamProps) => {
                         </tr>
                     </thead>
                     <tbody>
+                        {orders && orders.length === 0 ? (
+                            <tr className="border-b">
+                                <td colSpan={5} className="py-4 text-center text-gray-500">
+                                    No orders found.
+                                </td>
+                            </tr>
+                        ) : (
+                            <>
+                                {orders &&
+                                    orders.map((row: IOrderItem) => (
+                                        <tr
+                                            key={row._id}
+                                            className="p-regular-14 lg:p-regular-16 border-b "
+                                            style={{ boxSizing: 'border-box' }}>
+                                            <td className="min-w-[250px] py-4 text-primary-500">{row._id}</td>
+                                            <td className="min-w-[200px] flex-1 py-4 pr-4">{row.eventTitle}</td>
+                                            <td className="min-w-[150px] py-4">{row.buyer}</td>
+                                            <td className="min-w-[100px] py-4">
+                                                {formatDateTime(row.createdAt).dateTime}
+                                            </td>
+                                            <td className="min-w-[100px] py-4 text-right">
+                                                {formatPrice(row.totalAmount)}
+                                            </td>
+                                        </tr>
+                                    ))}
+                            </>
+                        )}
                     </tbody>
                 </table>
             </section>
