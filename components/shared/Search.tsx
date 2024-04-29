20087b37c68a4c6dd699d673187cec5aa2856408
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-29 20:10:43 +0300

    Update Search component

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,9 +1,38 @@
 'use client'
 
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { Input } from '../ui/input';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 
 const Search = ({ placeholder = 'Search title...' }: { placeholder?: string }) => {
+    const [query, setQuery] = useState('');
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            let newUrl = '';
+
+            if (query) {
+                newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'query',
+                    value: query
+                });
+            } else {
+                newUrl = removeKeysFromQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ['query']
+                })
+            }
+
+            router.push(newUrl, { scroll: false });
+        }, 300);
+
+        return () => clearTimeout(delayDebounceFn);
+    }, [query, searchParams, router]);
 
     return (
         <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
