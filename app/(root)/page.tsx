a96d678b25e85ee22845a3c10ea749c9e3708948
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-04-29 22:47:37 +0300

    Update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { getAllEvents } from '@/lib/actions/event.actions';
 import Collection from '@/components/shared/Collection';
+import Search from '@/components/shared/Search';
+import { SearchParamProps } from '@/types';
+import CategoryFilter from '@/components/shared/CategoryFilter';
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+    const page = Number(searchParams?.page) || 1;
+    const searchText = (searchParams?.query as string) || '';
+    const category = (searchParams?.category as string) || '';
 
     const events = await getAllEvents({
-        query: '',
-        category: '',
-        page: 1,
+        query: searchText,
+        category,
+        page,
         limit: 6
     });
 
@@ -39,6 +45,11 @@ export default async function Home() {
 
             <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
                 <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+
+                <div className="flex w-full flex-col gap-5 md:flex-row">
+                    <Search />
+                    <CategoryFilter />
+                </div>
 
                 <Collection
                     data={events?.data}
